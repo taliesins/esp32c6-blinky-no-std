dbf6bfc47f7da575fa0c8809034e85a016c58ada
Taliesin Sisson <taliesins@yahoo.com>
2024-09-15 10:17:01 +0100

    blinky works with async

--- src/main.rs
+++ src/main.rs
@@ -4,42 +4,56 @@
 
 use esp_backtrace as _;
 use esp_hal::{
-    delay::Delay,
     gpio::{Io, Level, Output},
     prelude::*,
+    timer::timg::TimerGroup,
 };
 use esp_println::{logger::init_logger_from_env, println};
 
 #[embassy_executor::task]
 async fn test_task() {
-    log::info!("Task");
-    println!("Taks")
+    log::info!("Test Task Starting");
+    loop {
+        log::info!("Test Task Running");
+        embassy_time::Timer::after(embassy_time::Duration::from_millis(2_000)).await;
+    }
 }
 
 #[main]
-async fn main(_s: embassy_executor::Spawner) {
-    init_logger_from_env();
+async fn main(spawner: embassy_executor::Spawner) {
+    //Fix log output in vscode
+    println!("\x1b[20h");
 
     let peripherals = esp_hal::peripherals::Peripherals::take();
     //let peripherals = esp_hal::init(esp_hal::Config::default()); //doesn't work ;<
+
+    // Setup Time Driver for running async sleeps
     let system = esp_hal::system::SystemControl::new(peripherals.SYSTEM);
     let clocks = esp_hal::clock::ClockControl::max(system.clock_control).freeze();
-    let delay = Delay::new(&clocks);
+
+    let timg0 = TimerGroup::new(peripherals.TIMG0, &clocks);
+    esp_hal_embassy::init(&clocks, timg0.timer0);
 
     // Set GPIO0 as an output, and set its state high initially.
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut led = Output::new(io.pins.gpio0, Level::High);
+    let mut led = Output::new(io.pins.gpio11, Level::High);
 
-    log::info!("Start");
+    init_logger_from_env();
 
-    _s.spawn(test_task()).ok();
+    log::info!("Main Starting");
+
+    spawner.spawn(test_task()).ok();
+
+    //Allow tasks to start before main loop is started
+    embassy_futures::yield_now().await;
 
     loop {
-        log::info!("Running");
+        log::info!("Main Running");
         led.toggle();
-        delay.delay_millis(500);
+        embassy_time::Timer::after(embassy_time::Duration::from_millis(1_000)).await;
         led.toggle();
-        // or using `fugit` duration
-        delay.delay(2.secs());
+        embassy_time::Timer::after(embassy_time::Duration::from_millis(1_000)).await;
     }
+
+    //log::info!("Finished");
 }
